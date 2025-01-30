ed4e16247907898f673c148d116d1b4312a076d3
Manya <121406344+manya3219@users.noreply.github.com>
2025-01-30 14:17:25 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserForm from "./components/UserForm";
-import HashRouter from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const App = () => {
   return (
